d1da671c7ac6abed79236a49570ea588dfab2c85
Rudra Pratap Singh <rudra@Rudras-Air.local>
2023-12-23 11:28:59 +0530

    pta nhi

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -5,9 +5,9 @@ const jwtPassword = 'secret';
 /**
  * Generates a JWT for a given username and password.
  *
- * @param {string} username - The username to be included in the JWT payload.
+ * @param {string} admin - The username to be included in the JWT payload.
  *                            Must be a valid email address.
- * @param {string} password - The password to be included in the JWT payload.
+ * @param {string} admin - The password to be included in the JWT payload.
  *                            Should meet the defined length requirement (e.g., 6 characters).
  * @returns {string|null} A JWT string if the username and password are valid.
  *                        Returns null if the username is not a valid email or
