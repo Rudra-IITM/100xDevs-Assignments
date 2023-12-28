52621b9f71cb8118f522ef39b1f96639d9430569
Rudra Pratap Singh <rudra@Rudras-Air.local>
2023-12-28 12:48:14 +0530

    03-mongo Done

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,40 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db/index")
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    await Admin.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    res.json({
+        message: "Admin created successfully"
+    })
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const newCourse = await Course.create({
+        title: req.body.title,
+        description: req.body.description,
+        imageLink: req.body.imageLink,
+        price: req.body.price
+    })
+    res.json({
+        message: 'Course created successfully',
+        courseId: newCourse._id
+    })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    const courses = await Course.find({});
+    res.json({
+        courses
+    });
 });
 
 module.exports = router;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,58 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db/index");
+const { consumers } = require("supertest/lib/test");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    await User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    res.json({
+        message: 'User created successfully'
+    })
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+    const courses = await Course.find({});
+    res.json({
+        courses
+    })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
+    const username = req.headers.username;
+    const courseId = req.params.courseId;
+    await User.updateOne({
+        username
+    },{
+        "$push": {
+            purchasedCourse: courseId
+        }
+    })
+    res.json({
+        message: "Course purchased successfully"
+    })
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const user = await User.find({
+        username: req.headers.username
+    });
+    const courses = await Course.find({
+        _id: {
+            "$in": user.purchasedCourse
+        }
+    })
+    res.json({
+        courses
+    })
 });
 
 module.exports = router
